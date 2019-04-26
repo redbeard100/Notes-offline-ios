d8ca8b9884484a1970b47428a2e546b31633976f
Soumil <LPTP233@joys-macbook-pro.local>
2019-04-26 12:49:38 +0530

    keyboard issue

--- Notes/AddNewFileViewController.swift
+++ Notes/AddNewFileViewController.swift
@@ -14,22 +14,26 @@ class AddNewFileViewController: UIViewController,UITextFieldDelegate,UITextViewD
     
     @IBOutlet weak var contentTextView: UITextView!
     
-    @IBOutlet weak var saveButton: UIBarButtonItem!
+    var saveButton: UIBarButtonItem!
     var textFieldFlag = 0
     var textViewInteraction = 0
     var flagUpdate = 0
     var indexNo:Int?
-    var keyboardheight: CGFloat = 0
+    var flag = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         contentTextView.delegate = self
         nameTextField.delegate = self
+       saveButton = UIBarButtonItem(title: "Save", style: .plain, target: self, action: #selector(saveButtonAction(_:)))
+        self.navigationItem.rightBarButtonItem  = saveButton
         saveButton.isEnabled = false
         nameTextField.layer.borderWidth = 3
         nameTextField.layer.borderColor = #colorLiteral(red: 0, green: 0.4784313725, blue: 1, alpha: 1)
         contentTextView.layer.borderWidth = 3
         contentTextView.layer.borderColor = #colorLiteral(red: 0, green: 0.4784313725, blue: 1, alpha: 1)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+//        contentTextViewHeight = contentTextView.frame.height
+//        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+//        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
         if let index = indexNo {
             nameTextField.text = DataModel.shared.name[index]
             contentTextView.text = DataModel.shared.content[index]
@@ -38,20 +42,29 @@ class AddNewFileViewController: UIViewController,UITextFieldDelegate,UITextViewD
     
     func textFieldDidBeginEditing(_ textField: UITextField) {
         saveButton.isEnabled = true
+        if flag == 0 {
+            flag = 1
+            contentTextView.frame.size = CGSize(width: contentTextView.frame.width, height: contentTextView.frame.height - view.frame.height/2.58)
+        }
 //        UIView.animate(withDuration: 0.5, animations: {
 //            self.view.frame.size = CGSize(width: self.view.frame.width, height: self.view.frame.height - self.keyboardheight)
 //        })
     }
     
     func textViewDidBeginEditing(_ textView: UITextView) {
-        textFieldFlag = 1
-        if textViewInteraction == 0 {
+//        textFieldFlag = 1
+//        if textViewInteraction == 0 {
+//            saveButton.isEnabled = true
+//            UIView.animate(withDuration: 0.5, animations: {
+//                self.view.frame.size = CGSize(width: self.view.frame.width, height: self.view.frame.height - self.keyboardheight)
+//            })
+//        }
+//        textViewInteraction += 1
+        if flag == 0 {
+            flag = 1
             saveButton.isEnabled = true
-            UIView.animate(withDuration: 0.5, animations: {
-                self.view.frame.size = CGSize(width: self.view.frame.width, height: self.view.frame.height - self.keyboardheight)
-            })
+            contentTextView.frame.size = CGSize(width: contentTextView.frame.width, height: contentTextView.frame.height - view.frame.height/2.58)
         }
-        textViewInteraction += 1
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -60,63 +73,44 @@ class AddNewFileViewController: UIViewController,UITextFieldDelegate,UITextViewD
         return false
     }
     
-    @IBAction func saveButtonAction(_ sender: UIBarButtonItem) {
+    @objc func saveButtonAction(_ sender: UIBarButtonItem) {
         textViewInteraction = 0
     if flagUpdate == 0 {
         if (nameTextField.text != "") && (contentTextView.text != "" ) {
             if  DataOperations.shared.saveData(contentData: contentTextView.text!, nameData: nameTextField.text!) {
-                let alert = UIAlertController(title: "Success", message: "File Saved", preferredStyle: UIAlertController.Style.alert)
-                alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { (action: UIAlertAction!) in
-                    let ShowFilesVC : ViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "ShowFilesVC") as! ViewController
-                    self.present(ShowFilesVC, animated: true, completion: nil)
-                }))
-                contentTextView.resignFirstResponder()
-                view.frame.size = CGSize(width:view.frame.width, height: view.frame.height + self.keyboardheight)
-                self.present(alert, animated: true, completion: nil)
+                alertPopUp(title: "Success", message: "File Saved", isSuccess: true)
+//                contentTextView.resignFirstResponder()
+//                view.frame.size = CGSize(width:view.frame.width, height: view.frame.height + self.keyboardheight)
             }else {
                 if textFieldFlag == 1 {
-                    let alert = UIAlertController(title: "Failed", message: "Failed to save data. Try Again", preferredStyle: UIAlertController.Style.alert)
-                    alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-                                    view.frame.size = CGSize(width:view.frame.width, height: view.frame.height + self.keyboardheight)
-                                    contentTextView.resignFirstResponder()
-                    self.present(alert, animated: true, completion: nil)
+                    alertPopUp(title: "Failed", message: "Failed to save data. Try Again", isSuccess: false)
+//                                    view.frame.size = CGSize(width:view.frame.width, height: view.frame.height + self.keyboardheight)
+//                                    contentTextView.resignFirstResponder()
                 }
             }
         }
             else {
-            let alert = UIAlertController(title: "Failed", message: "Please Enter Name and Content of the File", preferredStyle: UIAlertController.Style.alert)
-            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-                        view.frame.size = CGSize(width:view.frame.width, height: view.frame.height + self.keyboardheight)
-                        contentTextView.resignFirstResponder()
-            self.present(alert, animated: true, completion: nil)
-        }
+//                        view.frame.size = CGSize(width:view.frame.width, height: view.frame.height + self.keyboardheight)
+//                        contentTextView.resignFirstResponder()
+            alertPopUp(title: "Failed", message: "Please Enter Name and Content of the File", isSuccess: false)
+            }
         }
     else if (flagUpdate == 1) {
         if (nameTextField.text != "") && (contentTextView.text != "" ) {
             if  DataOperations.shared.updateData(name: nameTextField.text!, content: contentTextView.text!, index: indexNo!) {
-                let alert = UIAlertController(title: "Success", message: "File Updated", preferredStyle: UIAlertController.Style.alert)
-                alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { (action: UIAlertAction!) in
-                    let ShowFilesVC : ViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "ShowFilesVC") as! ViewController
-                    self.present(ShowFilesVC, animated: true, completion: nil)
-                }))
-                contentTextView.resignFirstResponder()
-                view.frame.size = CGSize(width:view.frame.width, height: view.frame.height + self.keyboardheight)
-                self.present(alert, animated: true, completion: nil)
+                alertPopUp(title: "Success", message: "File Updated", isSuccess: true)
+//                contentTextView.resignFirstResponder()
+//                view.frame.size = CGSize(width:view.frame.width, height: view.frame.height + self.keyboardheight)
             }else {
-                let alert = UIAlertController(title: "Failed", message: "Failed to update data. Try Again", preferredStyle: UIAlertController.Style.alert)
-                alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-                contentTextView.resignFirstResponder()
-                view.frame.size = CGSize(width:view.frame.width, height: view.frame.height + self.keyboardheight)
-                self.present(alert, animated: true, completion: nil)
+                alertPopUp(title: "Failed", message: "Failed to update data. Try Again", isSuccess: false)
+//                view.frame.size = CGSize(width:view.frame.width, height: view.frame.height + self.keyboardheight)
             }
         }
         else {
             if textFieldFlag == 1 {
-                let alert = UIAlertController(title: "Failed", message: "Please Enter Name and Content of the File", preferredStyle: UIAlertController.Style.alert)
-                alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-                contentTextView.resignFirstResponder()
-                view.frame.size = CGSize(width:view.frame.width, height: view.frame.height + self.keyboardheight)
-                self.present(alert, animated: true, completion: nil)
+                alertPopUp(title: "Failed", message: "Please Enter Name and Content of the File", isSuccess: false)
+//                contentTextView.resignFirstResponder()
+//                view.frame.size = CGSize(width:view.frame.width, height: view.frame.height + self.keyboardheight)
                     }
                 }
         }
@@ -124,13 +118,38 @@ class AddNewFileViewController: UIViewController,UITextFieldDelegate,UITextViewD
     }
 
     
-    @objc func keyboardWillShow(notification: NSNotification) {
-        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
-            keyboardheight = keyboardSize.height
+//    @objc func keyboardWillShow(notification: NSNotification) {
+//        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+//            keyboardheight = keyboardSize.height
+//        }
+//    }
+    
+//    func setTextcontentTextViewHeight() {
+//        contentTextView.frame.size = CGSize(width: contentTextView.frame.width, height: contentTextView.frame.height - keyboardheight)
+//    }
+    
+    func alertPopUp(title: String, message: String, isSuccess: Bool) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        if isSuccess {
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { (action: UIAlertAction!) in
+                self.navigationController?.popToRootViewController(animated: true)
+            }))
+        }else {
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         }
+        self.present(alert, animated: true, completion: nil)
     }
     
-    func setTextViewHeight() {
-        contentTextView.frame.size = CGSize(width: contentTextView.frame.width, height: contentTextView.frame.height - keyboardheight)
+//    @objc func keyboardWillShow(notification: NSNotification) {
+//        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+//            keyboardheight = keyboardSize.height
+//            }
+//        }
+////
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if flag == 1 {
+            contentTextView.frame.size = CGSize(width: contentTextView.frame.width, height: contentTextView.frame.height + view.frame.height/2.58)
+        }
+        flag = 0
     }
 }
